4a8ee6a1b2d2d40518f68fd2103de20f2d18cfee
douglasfcsoares <douglasfcsoares@gmail.com>
2025-08-13 21:46:10 -0300

    Métodos de array, map()

---
+++ metodos_de_arrays/map.js
@@ -0,0 +1,23 @@
+// O método map() chama a função callback recebida por parâmetro para cada elemento do array original, em ordem, e constrói um novo array com base nos retornos de cada chamada. E no final, devolve o novo array.
+
+const products = ["teclado", "mouse", "monitor", "gabinete"];
+
+// Percorrendo os itens do array.
+products.map(product => {
+  console.log(product);
+});
+
+// sintaxe reduzida.
+products.map(product => console.log(product));
+
+// Utilizando o novo objeto retornado.
+const formatted = products.map(product => {
+  // return product.toUpperCase();
+
+  return {
+    id: Math.random(),
+    description: product.toUpperCase()
+  };
+});
+
+console.log(formatted); // ["TECLADO", "MOUSE", "MONITOR", "
