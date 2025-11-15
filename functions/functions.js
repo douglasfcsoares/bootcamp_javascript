ae889f9a9bdcbd100f6d4154112ef5e6964b2b68
douglasfcsoares <douglasfcsoares@gmail.com>
2025-11-15 20:20:31 -0300

    functions

---
+++ functions/functions.js
@@ -0,0 +1,18 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+const result = sum(2, 4);
+console.log("🚀 ~ result:", result);
+
+// Exemplo de arrow function
+const sumArrow = (a, b) => {
+  return a + b;
+};
+
+// ou de forma mais enxuta
+
+const multiply = (a, b) => a * b;
+
+const product = multiply(3, 5);
+console.log("🚀 ~ product:", product);
